4acd411f0d5489cd00f5e0f2dc4ecc57bf438e4b
13Mzawa2 <nishizawa-masahiro-ys@ynu.jp>
2015-07-26 16:51:43 +0900

    Phongシェーディングらしきものを実�

---
+++ common/inc/objloader.hpp
@@ -0,0 +1,49 @@
+#ifndef OBJLOADER_H
+#define OBJLOADER_H
+
+#pragma region Disable Waring C4996
+//
+// Disable Warning C4996
+//
+#ifndef _CRT_SECURE_CPP_OVERLOAD_STANDARD_NAMES
+#define _CRT_SECURE_CPP_OVERLOAD_STANDARD_NAMES 1
+#endif
+#ifndef _CRT_SECURE_CPP_OVERLOAD_SECURE_NAMES
+#define _CRT_SECURE_CPP_OVERLOAD_SECURE_NAMES 1
+#endif
+#ifndef _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS 1
+#endif
+#pragma endregion
+
+#include <vector>
+#include <stdio.h>
+#include <string>
+#include <cstring>
+
+#include <glm/glm.hpp>
+
+typedef struct Object{
+	std::vector<glm::vec3> vertices;
+	std::vector<glm::vec2> uvs;
+	std::vector<glm::vec3> normals;
+};
+
+bool loadOBJ(
+	const char * path, 
+	std::vector<glm::vec3> & out_vertices, 
+	std::vector<glm::vec2> & out_uvs, 
+	std::vector<glm::vec3> & out_normals
+);
+
+bool loadOBJ(const char *path, Object &obj);
+
+bool loadAssImp(
+	const char * path, 
+	std::vector<unsigned short> & indices,
+	std::vector<glm::vec3> & vertices,
+	std::vector<glm::vec2> & uvs,
+	std::vector<glm::vec3> & normals
+);
+
+#endif
